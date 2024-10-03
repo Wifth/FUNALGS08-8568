45bf1eda1fab20661341032d1771efbc98ae5382
bojorquezja <bojorquez.ja@gmail.com>
2024-10-03 10:44:36 +0200

    Primer commit

---
+++ funciones/Perro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funciones {
+    internal class Perro {
+        public string Nombre { get; set; }
+
+        public static string Ladrar() {
+            int a = 5;
+            return "Guau";
+        }
+
+        public static void Ladrar(string lad) {
+            Console.WriteLine(lad);
+            
+        }
+    }
+}
---
+++ funciones/Program.cs
@@ -0,0 +1,49 @@
+﻿using funciones;
+
+namespace Program {
+    class TestClass {
+        static void Main(string[] args) {
+
+            int Azar() {
+                Random r = new Random();
+                return r.Next(1, 6 + 1);
+            }
+
+            int Suma(int d1, int d2) {
+                int x = d1 + d2;
+                return x;
+            }
+
+            void Imprime(int texto) {
+                Console.WriteLine(texto);
+            }
+
+            int a = 5;
+            int b = 6;
+
+            //int c = a + b;
+            int c = Suma(a, b);
+
+            Imprime(c);
+            //Console.WriteLine(c);
+
+            b = 10;
+
+            int d = a + b;
+
+            Console.WriteLine(d);
+
+            double z = 100.0;
+            float y = (float)100.0;
+
+            Console.WriteLine(Azar());
+
+
+            //Perro p = new Perro();
+            //p.Ladrar("Auuuuu");
+
+            Perro.Ladrar("Auuuuu");
+
+        }
+    }
+}
